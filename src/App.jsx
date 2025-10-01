1d3f62c29f01a154027e7f9506ededc4e372c7af
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-01 19:15:38 -0300

    importando inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './Style.css';
-import Login from './components/Login';
+import Inicio from './components/Inicio';
 
 function App() {
   return (
     <>
-      <Login />    
+      <Inicio />
     </>
   )
 }
