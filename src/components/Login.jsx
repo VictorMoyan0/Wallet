e19a0f0b2cd99476a4c399961d3de8b00a50d2ea
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-01 18:46:03 -0300

    se creo el login

---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <form action="#" method="#">
+            <input type="text" name="usuario" />
+            <input type="password" name="clave" />
+            <button type="submit">Enviar</button>
+        </form>
+    );
+}
