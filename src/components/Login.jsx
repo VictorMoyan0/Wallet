61e1b91a557ce8272632782fdb20c8a42eb64684
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-01 18:56:54 -0300

    se agrego btn registrar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,14 @@
 function Login() {
     return (
-        <form action="#" method="#">
-            <input type="text" name="user" placeholder="Usuario"/>
-            <input type="password" name="passwd" placeholder="Contraseña"/>
-            <button type="submit">Enviar</button>
-        </form>
+        <div className="login-container">
+            <h2>Iniciar Sesión</h2>
+            <form action="#" method="#">
+                <input type="text" name="user" placeholder="Usuario"/>
+                <input type="password" name="passwd" placeholder="Contraseña"/>
+                <button type="submit">Enviar</button>
+            </form>
+            <button className="btn-register">Registrarse</button>
+        </div>
     );
 }
 export default Login;
