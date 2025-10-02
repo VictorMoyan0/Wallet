605f4e6f5689905dd0a731a14480d6c1a1bb5972
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-02 16:08:48 -0300

    se agrego el titulo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,7 @@ function Menu() {
     return (
         <>
             <div className="menu-options">
+                <h2>Mi Billetera</h2>
                 <p>Usuario: {user}</p>
                 <p>Saldo: ARS {saldo}</p>
                 <button>Depositar</button>
