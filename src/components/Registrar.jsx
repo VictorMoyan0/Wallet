4cea9b05a9fc5538ecb6c082657feca63144e403
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-02 11:05:52 -0300

    conectando registrar con el server

--- src/components/Registrar.jsx
+++ src/components/Registrar.jsx
@@ -2,10 +2,35 @@ import { useNavigate } from "react-router-dom";
 
 function Registrar() {
     const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+    e.preventDefault(); // evita recargar la página
+    const user = e.target.user.value;
+    const passwd = e.target.passwd.value;
+
+    try {
+      const res = await fetch("http://localhost:3001/registrar", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user, passwd }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        alert(data.mensaje);
+        navigate("/"); // redirige al login después de registrar
+      } else {
+        alert(data.error);
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Error al conectarse al servidor");
+    }
+  };
     return (
         <div className="register-container">
             <h2>Registrarse</h2>
-            <form action="#" method="#">
+            <form onSubmit={handleSubmit}>
                 <input type="text" name="user" placeholder="Usuario"/>
                 <input type="password" name="passwd" placeholder="Contraseña"/>
                 <button type="submit">Enviar</button>
