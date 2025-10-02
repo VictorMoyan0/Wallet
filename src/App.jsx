a89a75a037f8492a42f4fe475858359f4d83b59a
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-02 10:36:43 -0300

    borrando cosas demas

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Registrar from "./components/Registrar";
 import './Style.css';
-import Inicio from './components/Inicio';
 
 function App() {
   return (
     <>
-      <Inicio />
+      <Login />
     </>
   )
 }
--- src/components/Inicio.jsx
+++
@@ -1,7 +0,0 @@
-import Login from './Login';
-function Inicio(){
-    return (
-        <Login />
-    );
-}
-export default Inicio;
