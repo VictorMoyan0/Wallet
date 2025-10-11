964dff8477a3f3061fef121ac62e3e950894ee79
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-11 16:13:07 -0300

    define "/deposit"

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Menu from "./components/Menu";
+import Deposit from "./components/Deposit";
 import './Style.css';
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         {/* Página de menú (después del login) */}
         <Route path="/menu" element={<Menu />} />
+        {/* Página de depósito */}
+        <Route path="/deposit" element={<Deposit />} />
       </Routes>
     </HashRouter>
   )
