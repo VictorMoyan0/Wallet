356c1806eec1c28bb03a6b81db3bedac886a9b7e
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-11 10:18:46 -0300

    add 'import React'

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"; 
 import { useLocation, useNavigate  } from "react-router-dom";
 import useAutoLogout from "./Hook";
 import ButtonLogout from "./Buttonlogout";
