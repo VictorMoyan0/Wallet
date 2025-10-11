3a6101a1c5cff35d6078748786fc696397ace20f
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-11 11:24:23 -0300

    add input the first name 'd last name

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -28,7 +28,8 @@ function Register() {
         <div className="register-container">
             <h2>Registrarse</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="user" placeholder="Usuario"/>
+                <input type="text" name="first-name" placeholder="Nombre"/>
+                <input type="text" name="last-name" placeholder="Apellido"/>
                 <input type="password" name="passwd" placeholder="Contraseña"/>
                 <button type="submit">Enviar</button>
             </form>
