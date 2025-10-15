78baea2fd8a2004127f252ccc443de151ec3a4a3
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-15 17:00:29 -0300

    add conts

--- src/components/Withdraw.jsx
+++ src/components/Withdraw.jsx
@@ -1,7 +1,39 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
+
 function Withdraw() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const user = location.state?.user;
+
+    const [amount, setAmount] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleWithdraw = async () => {
+        if (!amount || isNaN(amount)) {
+        setMessage("Ingrese un monto válido.");
+        return;
+    }
+    try {
+      const res = await fetch("http://localhost:3001/withdraw", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user: user.user, amount }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+            setMessage(data.error);
+            return;
+        }
+
+      setMessage(`Retiro exitoso. Nuevo saldo: ARS ${data.balance}`);
+      localStorage.setItem("user", JSON.stringify({ ...user, balance: data.balance }));
+      setTimeout(() => navigate("/menu", { state: { user: { ...user, balance: data.balance } } }), 1500);
+    } catch (err) {
+        setMessage("Error al conectar con el servidor.");
+    }
+    };
     return (
         <div>
             
