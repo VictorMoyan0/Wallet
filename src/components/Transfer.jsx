64223ca27c65bd7a5de006d848eb4bd66190cb66
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-10-15 16:19:14 -0300

    add inputs

--- src/components/Transfer.jsx
+++ src/components/Transfer.jsx
@@ -1,7 +1,25 @@
 function Transfer(){
     return (
         <div className="transfer-container">
-            
+            <form onSubmit={handleTransfer}>
+                <input
+                    type="text"
+                    placeholder="Alias o CBU del destinatario"
+                    value={to}
+                    onChange={(e) => setTo(e.target.value)}
+                    required
+                />
+                <input
+                    type="number"
+                    placeholder="Monto a transferir"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    required
+                />
+                <button type="submit">Enviar</button>
+            </form>
+            {message && <p>{message}</p>}
+            <button onClick={() => navigate("/menu", { state: { user } })}>Volver</button>
         </div>
     );
 }
