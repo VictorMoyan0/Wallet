a6c25287aa49c01750a70928f5f48cca2742e526
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-11-15 09:17:11 -0300

    fix import

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"; 
 import { useLocation, useNavigate  } from "react-router-dom";
 import useAutoLogout from "./Hook";
-import ButtonLogout from "./Buttonlogout";
+import ButtonLogout from "./ButtonLogout";
 
 
 function Menu() {
